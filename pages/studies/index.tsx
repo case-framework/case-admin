39d0378aebb78f29c6166e217b008c02991b32a4
phev8 <phev8@users.noreply.github.com>
2023-04-02 16:30:59 +0200

    use new fetcher on studies page

--- pages/studies/index.tsx
+++ pages/studies/index.tsx
@@ -1,6 +1,7 @@
+import { ApiError, AuthAPIFetcher } from '@/utils/server/fetcher';
 import { signIn } from 'next-auth/react';
 import Link from 'next/link';
-import useSWR, { Fetcher } from 'swr';
+import useSWR from 'swr';
 
 interface Study {
     key: string;
@@ -11,27 +12,9 @@ interface Studies {
     studies: Study[];
 }
 
-interface ApiError extends Error {
-    info: any;
-    status: number;
-}
-
-
-const StudyListFetcher: Fetcher<Studies, string> = async () => {
-    const res = await fetch(`/api/studies`)
-    if (!res.ok) {
-        const error = new Error('An error occurred while fetching the data.') as ApiError;
-        // Attach extra info to the error object.
-        error.info = await res.json()
-        error.status = res.status
-        throw error
-    }
-    return res.json()
-};
-
 
 export default function Studies() {
-    const { data: studyResponse, error, isLoading } = useSWR<Studies, ApiError>('studies', StudyListFetcher);
+    const { data: studyResponse, error, isLoading } = useSWR<Studies, ApiError>('/api/studies', AuthAPIFetcher);
     console.log(studyResponse, error, isLoading)
 
     if (error?.status === 401) {
