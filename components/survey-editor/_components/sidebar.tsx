af45da5c4fa74a7caf71d10f9be396497c841abd
phev8 <phev8@users.noreply.github.com>
2025-06-30 10:33:30 +0200

    sidebar items

--- components/survey-editor/_components/sidebar.tsx
+++ components/survey-editor/_components/sidebar.tsx
@@ -1,33 +1,61 @@
 import { Button } from "@/components/ui/button";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
-import { Plus } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Airplay, FileCog, ListTree } from "lucide-react";
+
+const SidebarItem: React.FC<{
+    icon: React.ReactNode
+    label: string
+    isActive: boolean
+    onClick: () => void
+}> = ({ icon, label, isActive, onClick }) => {
+    return <li>
+        <Tooltip
+            delayDuration={0}
+        >
+            <TooltipTrigger asChild>
+                <Button
+                    variant={isActive ? "secondary" : "ghost"}
+                    size="icon"
+                    onClick={onClick}
+                    className={cn('hover:bg-neutral-200', isActive && "bg-neutral-200 ")}
+                >
+                    {icon}
+                </Button>
+            </TooltipTrigger>
+            <TooltipContent
+                side="right"
+                align="center"
+            >
+                <p>{label}</p>
+            </TooltipContent>
+        </Tooltip>
+    </li>
+}
 
 const EditorSidebar: React.FC = () => {
-    return <div className="flex flex-col border-r border-border bg-blue-50/50 p-2">
-        <ul>
-            <li>
-                <Tooltip
-                    delayDuration={0}
-                >
-                    <TooltipTrigger asChild>
-                        <Button
-                            variant="ghost"
-                            size="icon"
+    return <div className="flex flex-col border-r border-border bg-sidebar p-1">
+        <ul className="space-y-1.5">
+            <SidebarItem
+                icon={<FileCog className="size-6" />}
+                label="Survey properties"
+                onClick={() => { }}
+                isActive={false}
+            />
+            <SidebarItem
+                icon={<ListTree className="size-6" />}
+                label="Item Editor"
+                onClick={() => { }}
+                isActive={true}
+            />
+            <SidebarItem
+                icon={<Airplay className="size-6" />}
+                label="Simulator"
+                onClick={() => { }}
+                isActive={false}
+            />
 
-                        >
-                            <Plus className="size-4" />
-                        </Button>
-                    </TooltipTrigger>
-                    <TooltipContent
-                        side="right"
-                        align="center"
-                    >
-                        <p>
-                            Add a new survey
-                        </p>
-                    </TooltipContent>
-                </Tooltip>
-            </li>
+
         </ul>
     </div>
 }
