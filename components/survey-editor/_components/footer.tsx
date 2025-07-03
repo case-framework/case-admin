54093392a5a25d02ada1b219a0655e344800a00c
phev8 <phev8@users.noreply.github.com>
2025-07-03 11:11:01 +0200

    Refactor SurveyEditor component structure by introducing EditorMain for improved layout management. Add CSS styles for the survey editor, enhancing visual consistency and background patterns. Update EditorFooter to utilize dynamic styling for better responsiveness.

--- components/survey-editor/_components/footer.tsx
+++ components/survey-editor/_components/footer.tsx
@@ -1,8 +1,22 @@
 
 
 const EditorFooter: React.FC = () => {
-    return <footer className="h-(--footer-height) bg-blue-50/50 border-t border-blue-400">
-        todo: footer
+    return <footer className="h-(--footer-height) bg-(--footer-bg-color) text-(--footer-text-color)">
+        <div className="flex items-center justify-between h-full text-sm px-4">
+            <div className="flex items-center gap-4 h-full">
+                <div className="px-2 hover:bg-(--footer-text-color)/10 h-full flex items-center">
+                    history stack
+                </div>
+                <div className="px-2 hover:bg-(--footer-text-color)/20">
+                    issues
+                </div>
+            </div>
+            <div className="">
+                <div className="px-2">
+                    help
+                </div>
+            </div>
+        </div>
     </footer>
 }
 
--- components/survey-editor/index.tsx
+++ components/survey-editor/index.tsx
@@ -1,8 +1,9 @@
-import { SidebarInset } from "../ui/sidebar"
 import { TooltipProvider } from "../ui/tooltip";
 import EditorFooter from "./_components/footer";
 import EditorHeader from "./_components/header";
+import EditorMain from "./_components/main";
 import EditorSidebar from "./_components/sidebar";
+import "./survey-editor.css";
 
 interface SurveyEditorProps {
 }
@@ -10,32 +11,13 @@ interface SurveyEditorProps {
 
 const SurveyEditor: React.FC<SurveyEditorProps> = (props) => {
 
-    return <div className="[--header-height:calc(--spacing(10))] [--footer-height:calc(--spacing(8))]">
+    return <div className="survey-editor">
         <TooltipProvider>
             <div className="flex flex-col h-screen w-screen overflow-hidden">
                 <EditorHeader />
                 <div className="flex flex-1 overflow-hidden">
                     <EditorSidebar />
-                    <main className="grow">
-                        <div className="flex flex-col h-full overflow-hidden">
-                            <div className="flex-1 overflow-y-auto p-4">
-                                <div className="space-y-4">
-
-                                    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                                        <div className="bg-muted/50 aspect-video rounded-xl" />
-                                        <div className="bg-muted/50 aspect-video rounded-xl" />
-                                        <div className="bg-muted/50 aspect-video rounded-xl" />
-                                    </div>
-                                    <div className="bg-muted/50 flex-1 rounded-xl h-96" />
-                                    <div className="bg-muted/50 flex-1 rounded-xl h-96" />
-                                    <div className="bg-muted/50 flex-1 rounded-xl h-96" />
-                                    <div className="bg-muted/50 flex-1 rounded-xl h-96" />
-                                    <div className="bg-muted/50 flex-1 rounded-xl h-96" />
-                                    <div className=" flex-1 rounded-xl h-96 bg-red-50/50 border border-red-400" />
-                                </div>
-                            </div>
-                        </div>
-                    </main>
+                    <EditorMain />
                 </div>
                 <EditorFooter />
             </div>
