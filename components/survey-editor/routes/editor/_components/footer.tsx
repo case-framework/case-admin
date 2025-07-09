283f3464671e6532c9db9646aff772b49bb6d748
phev8 <phev8@users.noreply.github.com>
2025-07-09 11:47:07 +0200

    Implement HistoryStack component and integrate it into EditorFooter
    - Added a new HistoryStack component to display recent changes and allow users to navigate through the history of edits.
    - Updated EditorFooter to include the HistoryStack, replacing the previous static history stack placeholder.
    - Enhanced the footer's styling for better layout and responsiveness.

--- components/survey-editor/routes/editor/_components/footer.tsx
+++ components/survey-editor/routes/editor/_components/footer.tsx
@@ -1,12 +1,11 @@
+import HistoryStack from "./history-stack";
 
 
 const EditorFooter: React.FC = () => {
     return <footer className="h-(--footer-height) bg-(--footer-bg-color) text-(--footer-text-color)">
-        <div className="flex items-center justify-between h-full text-sm px-4">
+        <div className="flex items-center justify-between h-full text-sm ps-4 pe-4 overflows-y-hidden">
             <div className="flex items-center gap-4 h-full">
-                <div className="px-2 hover:bg-(--footer-text-color)/10 h-full flex items-center">
-                    history stack
-                </div>
+                <HistoryStack />
                 <div className="px-2 hover:bg-(--footer-text-color)/20">
                     issues
                 </div>
