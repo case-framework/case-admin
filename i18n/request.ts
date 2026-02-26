abdc1e0e4b37ca0071d660085ea1e58158919737
Kevin Brendel <brendel@coneno.com>
2026-02-26 18:30:52 +0100

    feat: simplify getRequestConfig by calling getLocale

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,15 +1,8 @@
 import { getRequestConfig } from 'next-intl/server';
-import { cookies } from 'next/headers';
-import { LOCALES } from './locales';
-import { isSupportedLocale, detectBrowserLocale } from './utils';
+import { getLocale } from './actions';
 
 export default getRequestConfig(async () => {
-    const store = await cookies();
-    const cookieLocale = store.get('locale')?.value;
-    const locale = cookieLocale && isSupportedLocale(cookieLocale)
-        ? cookieLocale as LOCALES
-        : await detectBrowserLocale();
-
+    const locale = await getLocale();
     return {
         locale,
         messages: (await import(`./messages/${locale}.json`)).default
